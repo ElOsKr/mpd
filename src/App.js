d361d4e38b321ed3fe7f2fecd97188a88d010daa
Oscar Alcivar <eloskr16@gmail.com>
2023-02-27 15:18:07 +0100

    routes finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Favorites  from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const router = createBrowserRouter( createRoutesFromElements(
+    <>
+      <Route path="/" element={ <Home/> }/>
+      <Route path = "/search" element = {<Search />}/>
+      <Route path = "/favorites" element = {<Favorites />}/>
+      <Route path='*' element ={<NotFound />} />  
+    </>
+
+  ))
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
