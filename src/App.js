84475227623183380a33e70848d9bd9a148e01c2
Oscar <eloskr16@gmail.com>
2023-02-28 12:44:03 +0100

    navigation link fixed and starting navbar function

--- src/App.js
+++ src/App.js
@@ -3,29 +3,26 @@ import Home from './pages/Home';
 import Search from './pages/Search';
 import Favorites  from './pages/Favorites';
 import NotFound from './pages/NotFound';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
+import { Route, BrowserRouter, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 function App() {
-
-  const router = createBrowserRouter( createRoutesFromElements(
-    <>
-      <Route path="/" element={ <Home/> }/>
-      <Route path = "/search" element = {<Search />}/>
-      <Route path = "/favorites" element = {<Favorites />}/>
-      <Route path='*' element ={<NotFound />} />  
-    </>
-
-  ))
 
   return (
     <div 
     className='App'
     style={{backgroundColor: '#393E46'}}
     >
-      <Navbar />
-      <RouterProvider router={router}/>
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={ <Home/> }/>
+          <Route path = "/search" element = {<Search />}/>
+          <Route path = "/favorites" element = {<Favorites />}/>
+          <Route path='*' element ={<NotFound />} />           
+        </Routes>
+        <Footer />
+      </BrowserRouter> 
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavLink } from 'react-router-dom';
 
 
 function Navbar() {
 
+
+  const handleOpenMenu = () => {
+
+  }
+
+
   return (
     <AppBar position="static">
-      <Toolbar sx={{background: "#222831"}}>
-        <Typography variant="body1" color="white">MPD</Typography>
+      <Toolbar 
+        sx={{
+          background: "#222831",
+          justifyContent: 'space-between'
+        }}>
+        <Box>
+          <Typography color="white">MPD</Typography>
+        </Box>
+          <MenuIcon
+            sx={{
+              cursor: 'pointer',
+              display: {md: 'none'}
+            }}
+          />          
+        <Box
+          sx={{
+            display: {xs: 'none', sm: 'none', md: 'flex'},
+            justifyContent: 'space-evenly',
+            width: '350px'
+          }}
+        >
+          <NavLink to="/" 
+            className= { ( { isActive}) => `nav-link ${isActive? 'nav-link-active': ''}`}
+          >
+            Home
+          </NavLink>
+          <NavLink to="/search" 
+            className= { ( { isActive}) => `nav-link ${isActive? 'nav-link-active': ''}`}
+          >
+            Search
+          </NavLink>
+          <NavLink to="/favorites" 
+            className= { ( { isActive}) => `nav-link ${isActive? 'nav-link-active': ''}`}
+          >
+            Favorites
+          </NavLink>
+        </Box>
       </Toolbar>
     </AppBar>
   );
